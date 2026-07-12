537cb33d715d088a9cf247fb67038201e902529e
Miketryartd <mikeleano26@gmail.com>
2026-07-12 11:23:19 +0800

    initial design for register/ no logo yet

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -21,12 +21,19 @@ export default function Register(){
     return (
         <>
         <div>
-            <form method="POST" onSubmit={handleRegister}>
-                <input type="text" placeholder="Full Name" name="name" required onChange={(e) => setName(e.target.value)}></input>
-                <input type="email" placeholder="Email" name="email" required onChange={(e) => setEmail(e.target.value)}></input>
-                <input type="password" placeholder="Password" name="password"  onChange={(e) => setPassword(e.target.value)}required></input>
-                <button type="submit">Create account</button>
+
+           <div className="min-h-screen flex items-center justify-center bg-gray-100">
+            <div>
+                
+            </div>
+             <form className="flex flex-col gap-4 bg-white p-8 w-96 rounded-md shadow-md" method="POST" onSubmit={handleRegister}>
+                <input className="border-b border-t-none border-slate-800/40 outline-none text-black p-3" type="text" placeholder="Full Name" name="name" required onChange={(e) => setName(e.target.value)}></input>
+                <input className="border-b border-t-none border-slate-800/40 outline-none text-black p-3" type="email" placeholder="Email" name="email" required onChange={(e) => setEmail(e.target.value)}></input>
+                <input className="border-b border-t-none border-slate-800/40 outline-none text-black p-3" type="password" placeholder="Password" name="password"  onChange={(e) => setPassword(e.target.value)}required></input>
+                <button className="bg-red-400 text-white hover:bg-red-600 rounded-md cursor-pointer p-2" type="submit">Create account</button>
             </form>
+           </div>
+
         </div>
         </>
     )
