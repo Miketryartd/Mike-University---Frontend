0e4085c2a69de9628cfe2b07970458921599504d
Miketryartd <mikeleano26@gmail.com>
2026-07-15 18:45:51 +0800

    slight fix design on the search button and search input

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -18,9 +18,9 @@ export default function Search(){
         <>
         <div className="">
 
-          <div className="min-w-100 w-full flex-row flex">
-              <input className="w-full p-4 outline-none  border-b bg-white " type="text" onChange={(e) => setQuery(e.target.value)} placeholder="Search.." required name="search"></input>
-            <button className="cursor-pointer p-4 bg-red-400 text-white transition hover:bg-red-600 rounded-full " onClick={handleSearch}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-search"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M3 10a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" /><path d="M21 21l-6 -6" /></svg></button>
+          <div className="min-w-100 w-full flex-row flex relative">
+              <input className="w-full p-4 outline-none  border-b bg-white rounded-r-full " type="text" onChange={(e) => setQuery(e.target.value)} placeholder="Search.." required name="search"></input>
+            <button className="cursor-pointer absolute right-0 p-4 bg-red-400 text-white transition hover:bg-red-600 rounded-full " onClick={handleSearch}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-search"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M3 10a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" /><path d="M21 21l-6 -6" /></svg></button>
           </div>
 
           {results && results.length > 0 ? (
