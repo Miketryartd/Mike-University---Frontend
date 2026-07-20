f7ce13525d824db692ca252a09aa6d602e608d55
Miketryartd <mikeleano26@gmail.com>
2026-07-20 19:53:54 +0800

    upd schema

--- src/schemas/User.ts
+++ src/schemas/User.ts
@@ -1,12 +1,12 @@
 
 export interface User {
-    id: number;
+    id?: number;
     name?: string;
     email: string;
     password: string;
-    role: string;
- email_verified_at: string | null;
-  created_at: string;
-  updated_at: string;
+    role?: string;
+ email_verified_at?: string | null;
+  created_at?: string;
+  updated_at?: string;
   
 }
