be0564c8b0765fad885f22e33aae1b1a0c2503f4
Miketryartd <mikeleano26@gmail.com>
2026-07-22 19:02:41 +0800

    updated class schema

--- src/schemas/Class.ts
+++ src/schemas/Class.ts
@@ -1,6 +1,10 @@
 
 export interface Class {
-    class_name: string;
+    subject_name: string;
+    class_section?: string;
+    user_email?: string;
     class_code?: string;
     creator_id?: number;
+    created_at?: Date;
+    updated_at?: Date;
 }
