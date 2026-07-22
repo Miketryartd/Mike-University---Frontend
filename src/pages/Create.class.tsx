0c7f47b1afc7ff7680a9500f7725777b6196debf
Miketryartd <mikeleano26@gmail.com>
2026-07-22 19:02:23 +0800

    updated create class page with subject name and section

--- src/pages/Create.class.tsx
+++ src/pages/Create.class.tsx
@@ -2,18 +2,21 @@ import { useState } from "react"
 import { useClass } from "../hooks/useClass";
 
 export default function MakeClass(){
-   const [className, setClassName] = useState<string>("");
+   const [subjectName, setSubjectName] = useState<string>("");
+   const [sectionName, setSectionName] = useState<string>("");
    const {loading, error, createClass} = useClass();
 
    const handleClass = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (!className.trim()){
+    if (!subjectName.trim()){
             alert("Please enter a class name");
             return;
     }
       try{
         
-         const res = await createClass({class_name: className,});
+         const res = await createClass({subject_name: subjectName,
+            class_section: sectionName
+         });
          return res;
       } catch (err){
         console.error("Error making class", err);
@@ -26,7 +29,8 @@ export default function MakeClass(){
 
             <div>
                 <form method="POST" onSubmit={handleClass}>
-                    <input type="text" placeholder="Class Name" onChange={(e) => setClassName(e.target.value)}></input>
+                    <input type="text" placeholder="Subject Name" onChange={(e) => setSubjectName(e.target.value)}></input>
+                    <input type="text" placeholder="Class Section" onChange={(e) => setSectionName(e.target.value)}></input>
                     <button type="submit">Create class</button>
                 </form>
             </div>
