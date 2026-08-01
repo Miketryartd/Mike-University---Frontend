368bdd0b8e31c00c0e7fdd8aa01c6fc184d62edb
Miketryartd <mikeleano26@gmail.com>
2026-08-01 19:07:49 +0800

    fix recommended users response

--- src/api/api.user.ts
+++ src/api/api.user.ts
@@ -3,20 +3,21 @@ import { api } from "./client";
 export const apiUser = async (id: number) => {
   try {
     const res = await api.get(`api/profile/user/${id}`);
-    const d = res.data.user || res.data;
+    const d = res.data.user || res.data || [];
     return d;
   } catch (err) {
     console.error("Error fetching user with this id", err);
   }
 };
 
-
 export const apiRSUser = async () => {
   try{
     const res = await api.get(`api/user/recommended`);
-    const d = res.data.user || res.data;
+   
+    const d = res.data.user?.data || res.data?.data || [];
     return d;
   } catch (err){
     console.error("Error fetching recommended user", err);
+    return []; 
   }
 }
